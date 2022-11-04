2dc4bca9de01c20cec35e7f045e41ecd9fe03793
Sribarrow <sribarrow@gmail.com>
2022-11-04 20:52:44 +0000

    linting changes

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # https://chris-clem.hashnode.dev/kickstart-your-next-python-project-with-with-poetry-pre-commit-and-github-actions-ck8jb6t9i00jlphs1askkcori
+
+curl -sSL https://install.python-poetry.org | python -
---
+++ python_template_repo/main.py
@@ -0,0 +1,13 @@
+"""
+    This is the main module for the application
+"""
+from datetime import date
+
+
+def main() -> None:
+    today = date(2022, 1, 1)
+    print(today)
+
+
+if __name__ == "__main__":
+    main()
